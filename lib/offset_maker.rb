8714336e8c47d970314dc04bf47eb0b6585982ad
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-11 15:38:25 -0700

    Feat: Create #offset(date) and #set_offsets

--- lib/offset_maker.rb
+++ lib/offset_maker.rb
@@ -3,4 +3,20 @@ class OffsetMaker
   def initialize(date)
     @date = date
   end
-end 
+
+  def offset(date)
+    squared = ((date.to_i)**2).to_s
+    @last_four = squared[-4..-1]
+    # require "pry"; binding.pry
+  end
+
+  def set_offsets
+    offset(date)
+    @offsets = Hash.new
+    @offsets["A"] = @last_four[0]
+    @offsets["B"] = @last_four[1]
+    @offsets["C"] = @last_four[2]
+    @offsets["D"] = @last_four[3]
+    @offsets
+  end
+end
