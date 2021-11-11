656177bb564d9154c91ab2413a56ac26f8e45509
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-11 15:52:34 -0700

    Feat: Update class to have @keys hash that is updated in the #generate_four_keys to store the keys

--- lib/key_maker.rb
+++ lib/key_maker.rb
@@ -2,6 +2,7 @@ class KeyMaker
   attr_accessor :key
   def initialize(key = nil)
     @key = generate(key)
+    @keys = {}
     # @a_key = key[0..1]
     # @b_key = key[1..2]
     # @c_key = key[2..3]
@@ -18,9 +19,10 @@ class KeyMaker
   end
 
   def generate_four_keys(key)
-    a_key = key[0..1]
-    b_key = key[1..2]
-    c_key = key[2..3]
-    d_key = key[3..4]
+    @keys["A"] = (a_key = key[0..1])
+    @keys["B"] = (b_key = key[1..2])
+    @keys["C"] = (c_key = key[2..3])
+    @keys["D"] = (d_key = key[3..4])
+    @keys
   end
 end
