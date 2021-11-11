13a683dbbc978bd9a0f7bc2c76e2029d64d64f1b
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-11 15:36:45 -0700

    Test: Create and modify tests for generating offset digits and generating A-D offsests

--- spec/offset_maker_spec.rb
+++ spec/offset_maker_spec.rb
@@ -8,10 +8,14 @@ RSpec.describe OffsetMaker do
     offset_maker = OffsetMaker.new("040895")
   end
 
-  it 'generates the offsets from the date of transmission' do
+  it 'generates the offset digits from the date of transmission' do
     offset_maker = OffsetMaker.new("040895")
-    expect(offset_maker.offsets).to eq({"A" => "1",
-    "B" => "0", "C" => "2", "D" => "5"})
+    expect(offset_maker.offset("040895")).to eq("1025")
   end
 
+  it 'generates offsets A-D using the offset digits' do
+    offset_maker = OffsetMaker.new("040895")
+    expect(offset_maker.set_offsets).to eq({"A" => "1",
+    "B" => "0", "C" => "2", "D" => "5"})
+  end
 end
