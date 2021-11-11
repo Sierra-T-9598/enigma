37df3552bb1d60ba53ed3e90181f97cf1f8f9384
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-11 15:03:12 -0700

    Test: Keymaker can randomly generate a 5-digit key to use

--- spec/key_maker_spec.rb
+++ spec/key_maker_spec.rb
@@ -1,11 +1,21 @@
 require './lib/key_maker.rb'
 
 RSpec.describe KeyMaker do
-  before(:each) do
-    @key_maker = KeyMaker.new
-  end
 
   it 'exists' do
-    expect(@key_maker).to be_instance_of(KeyMaker)
-  end 
+    key_maker = KeyMaker.new("02715")
+    expect(key_maker).to be_instance_of(KeyMaker)
+  end
+
+  it 'has attributes' do
+    key_maker = KeyMaker.new("02715")
+    expect(key_maker.key).to eq("02715")
+  end
+
+  it 'can randomly generate a 5-digit key to use' do
+    key_maker_random = KeyMaker.new
+    key_maker_random.key
+    expect(key_maker_random.key).to be_instance_of(String)
+    expect(key_maker_random.key.length).to be(5)
+  end
 end
