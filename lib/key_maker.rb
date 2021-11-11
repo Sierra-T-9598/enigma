a92e5af06ae5d1ea4a52ddd050190cb737f87771
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-11 15:04:01 -0700

    Feat: Refine initialize method with default key method and move four keys to separate method

--- lib/key_maker.rb
+++ lib/key_maker.rb
@@ -1,4 +1,26 @@
 class KeyMaker
-  def initialize
+  attr_accessor :key
+  def initialize(key = nil)
+    @key = generate(key)
+    # @a_key = key[0..1]
+    # @b_key = key[1..2]
+    # @c_key = key[2..3]
+    # @d_key = key[3..4]
+    # require "pry"; binding.pry
   end
-end 
+
+  def generate(key)
+    if key == nil
+      @key = 5.times.map{rand(5)}.join
+    else
+      key
+    end
+  end
+
+  def generate_four_keys(key)
+    a_key = key[0..1]
+    b_key = key[1..2]
+    c_key = key[2..3]
+    d_key = key[3..4]
+  end
+end
