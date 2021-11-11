9132a631cfa28237b565b9e4e2144e3ecd3ed575
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-10 21:32:10 -0700

    Test: Create #encrypt and #decrypt tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,3 +1,5 @@
+require './spec_helper.rb'
+require 'date'
 require './lib/enigma.rb'
 
 RSpec.describe Enigma do
@@ -8,4 +10,18 @@ RSpec.describe Enigma do
   it 'exists' do
     expect(@enigma).to be_instance_of(Enigma)
   end
-end 
+
+  it 'can encrypt a message with a key and date' do
+    expected = {:encryption => "keder ohulw",
+                :key => "02715",
+                :date => "040895"}
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+  end
+
+  it 'can decrypt a message with a key and date' do
+    expected = {:encryption => "hello world",
+                :key => "02715",
+                :date => "040895"}
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected)
+  end
+end
