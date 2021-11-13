7c3b3119d2aaf9a557f259943285dfe8a01cff82
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-12 21:48:11 -0700

    Feat: Add #decrypt functionality

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,14 +12,13 @@ class Enigma
     @key_maker = KeyMaker.new(key)
     @offset_maker = OffsetMaker.new(date)
     @shift_finder = ShiftFinder.new(@key_maker.keys, @offset_maker.offsets)
-
     encrypted = {}
+
     shifts_array = @shift_finder.shifts.values.map {|value| value.to_i}
 
     encrypted_text = ""
     message.downcase!
     shift_loop = shifts_array.cycle(message.length)
-    # loop do
       message.chars.each_with_index do |letter, index|
         shift_letter_index = shift_loop.next
         new_index = @alphabet_array.index(letter) + shift_letter_index
@@ -27,21 +26,38 @@ class Enigma
         new_index -= 27 if new_index > 26
         new_letter = @alphabet_array[new_index]
         encrypted_text << new_letter.to_s
-        # shift_letter_index = shift_loop.rotate
-        # if encrypted_text.length == message.length
-        #   break
-        # end
       end
-
       encrypted_text
-      require "pry"; binding.pry
-    end
-
-
-    # encrypted[:key] = key
-    # encrypted[:date] = date
-    # require "pry"; binding.pry
+      encrypted[:encryption] = encrypted_text
+      encrypted[:key] = key
+      encrypted[:date] = date
+      encrypted
+  end
 
   def decrypt(ciphertext, key, date)
+    @key_maker = KeyMaker.new(key)
+    @offset_maker = OffsetMaker.new(date)
+    @shift_finder = ShiftFinder.new(@key_maker.keys, @offset_maker.offsets)
+    encrypted = {}
+
+    shifts_array = @shift_finder.shifts.values.map {|value| value.to_i}
+
+    encrypted_text = ""
+    ciphertext.downcase!
+    shift_loop = shifts_array.cycle(ciphertext.length)
+      ciphertext.chars.each_with_index do |letter, index|
+        shift_letter_index = shift_loop.next
+        new_index = @alphabet_array.reverse.index(letter) + shift_letter_index
+        new_index -= 54 if new_index > 54
+        new_index -= 27 if new_index > 26
+        new_letter = @alphabet_array.reverse[new_index]
+        encrypted_text << new_letter.to_s
+      end
+      encrypted_text
+      encrypted[:encryption] = encrypted_text
+      encrypted[:key] = key
+      encrypted[:date] = date
+      encrypted
+      # require "pry"; binding.pry
   end
 end
