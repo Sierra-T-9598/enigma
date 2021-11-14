2674e025b9d6c64cccb691d0aad891b179c1497d
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-13 21:00:18 -0700

    Fix argument error of last test

--- spec/key_maker_spec.rb
+++ spec/key_maker_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe KeyMaker do
 
   it 'can generate four new keys' do
     key_maker = KeyMaker.new("02715")
-    expect(key_maker.generate_four_keys("02715")).to eq({"A" => "02",
+    expect(key_maker.generate_four_keys).to eq({"A" => "02",
       "B" => "27", "C" => "71", "D" => "15"})
   end
 
