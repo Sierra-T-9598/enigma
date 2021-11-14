e74de29ad31572603b50b0eacd32358b32c1bfac
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-14 11:19:16 -0700

    Refactor: Handle all index edgecases

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -19,8 +19,8 @@ class Enigma
         if @alphabet_array.include?(letter)
           shift_letter_index = shift_loop.next
           new_index = @alphabet_array.index(letter) + shift_letter_index
-          # new_index -= 108 if new_index > 108
-          # new_index -= 81 if new_index > 81
+          new_index -= 108 if new_index > 108
+          new_index -= 81 if new_index > 81
           new_index -= 54 if new_index > 54
           new_index -= 27 if new_index > 26
           new_letter = @alphabet_array[new_index]
@@ -52,8 +52,8 @@ class Enigma
         if @alphabet_array.include?(letter)
           shift_letter_index = shift_loop.next
           new_index = @alphabet_array.index(letter) - shift_letter_index
-          # new_index -= 108 if new_index > 108
-          # new_index -= 81 if new_index > 81
+          new_index += 108 if new_index < -81
+          new_index += 81 if new_index < -54
           new_index += 54 if new_index < -27
           new_index += 27 if new_index < 0
           new_letter = @alphabet_array[new_index]
