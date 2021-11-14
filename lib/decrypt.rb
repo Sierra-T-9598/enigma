335ec45b65eddbcddd12baaf67a0f5f333ee4bfb
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-13 21:28:47 -0700

    Feat: Creat CLI for decryption of an encrypted message in Enigma

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,15 +1,18 @@
-require './enigma.rb'
-
+require_relative './enigma.rb'
+require_relative './key_maker.rb'
+require_relative './offset_maker.rb'
+require_relative './shift_finder.rb'
+enigma = Enigma.new
 reader = File.open(ARGV[0], "r")
-encrypted_text = handle.read
-handle.close
+encrypted_text = reader.read
+reader.close
 
-### decrypted_text = encrypted_text.METHOD to decrypt
-# writer = File.open(ARGV[1], "w")
-# writer.write(decrypted_text)
-# writer.close
+decrypted_text = enigma.decrypt(encrypted_text.chomp, ARGV[2], ARGV[3])
+writer = File.open(ARGV[1], "w")
+writer.write(decrypted_text[:decryption])
+writer.close
 
-puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
+puts "Created '#{ARGV[1]}' with the key #{decrypted_text[:key]} and date #{decrypted_text[:date]}"
 
 ###COMMAND LINE:
 # ruby decrypt.rb encrypted.txt decrypted.txt key date
