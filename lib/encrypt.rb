c37075f47be437eaa374265df34394bef97418f2
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-13 21:02:04 -0700

    Feat: Create CLI for encryption of a message file using Enigma

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,15 +1,21 @@
-require './enigma.rb'
+require_relative './enigma'
+require_relative './key_maker.rb'
+require_relative './offset_maker.rb'
+require_relative './shift_finder.rb'
+enigma = Enigma.new
 
 reader = File.open(ARGV[0], "r")
-incoming_text = handle.read
-handle.close
+# Dir.chdir(File.dirname('lib'))
+incoming_text = reader.read
+reader.close
 
-### encrypted_text = incoming_text.METHOD
-# writer = File.open(ARGV[1], "w")
-# writer.write(encrypted_text)
-# writer.close
+encrypted_text = enigma.encrypt(incoming_text.chomp)
+writer = File.open(ARGV[1], "w")
+# require "pry"; binding.pry
+writer.write(encrypted_text[:encryption])
+writer.close
 
-puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
+puts "Created #{ARGV[1]} with the key #{encrypted_text[:key]} and date #{encrypted_text[:date]}"
 
 ###COMMAND LINE:
 # ruby encrypt.rb message.txt encrypted.txt
